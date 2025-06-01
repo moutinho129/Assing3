0b2f5ab9c9aa562fc886e71276fce6be062ee90b
Nuno Moutinho <nunomoutinho@ua.pt>
2025-06-01 19:01:33 +0100

    build_test(pasta para Unity Testing)

---
+++ build_test/CMakeFiles/test_runner.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_runner.dir/home/moutinho/Desktop/assing3/unity/unity.c.o"
+  "CMakeFiles/test_runner.dir/home/moutinho/Desktop/assing3/unity/unity.c.o.d"
+  "CMakeFiles/test_runner.dir/mock_cmd.c.o"
+  "CMakeFiles/test_runner.dir/mock_cmd.c.o.d"
+  "CMakeFiles/test_runner.dir/mock_task.c.o"
+  "CMakeFiles/test_runner.dir/mock_task.c.o.d"
+  "CMakeFiles/test_runner.dir/mock_zephyr.c.o"
+  "CMakeFiles/test_runner.dir/mock_zephyr.c.o.d"
+  "CMakeFiles/test_runner.dir/unitycmd.c.o"
+  "CMakeFiles/test_runner.dir/unitycmd.c.o.d"
+  "test_runner"
+  "test_runner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
